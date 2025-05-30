51dc4b8fdca317e51aa9057ed6e683bfac844d29
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2025-05-30 15:03:39 -0300

    inicio modelo salesOrder

---
+++ SalesService/Infrastructure/Models/SalesOrder/SalesOrder.cs
@@ -0,0 +1,19 @@
+﻿namespace SalesService.Infrastructure.Models.SalesOrder
+{
+    public class SalesOrder
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int CustomerId { get; set; }
+        public int SellerId { get; set; }
+        public int PriceListId { get; set; }
+        public int? TransportId { get; set; }
+        public int WarehouseId { get; set; }
+        public string? Notes { get; set; }
+        public bool IsDelivered { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Total { get; set; }
+        public List<SalesOrder_Article> Articles { get; set; } = new();
+    }
+}
---
+++ SalesService/Infrastructure/Models/SalesOrder/SalesOrder_Article.cs
@@ -0,0 +1,16 @@
+﻿namespace SalesService.Infrastructure.Models.SalesOrder
+{
+    public class SalesOrder_Article
+    {
+        public int Id { get; set; }
+        public int SalesOrderId { get; set; }
+        public int ArticleId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountPercent { get; set; }
+        public decimal IVA { get; set; }
+        public decimal Total { get; set; }
+
+        public SalesOrder SalesOrder { get; set; } = null!;
+    }
+}
