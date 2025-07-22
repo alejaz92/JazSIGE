f0ccefcd70d6746adc6a524f0335f788baf2590b
Alejandro Jazmatie <alejandro.jaz92@gmail.com>
2025-07-22 11:30:06 -0300

    prueba actions 5

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -11,6 +11,8 @@ builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 builder.Logging.AddAzureWebAppDiagnostics();
+// Este comentario es para disparar el workflow
+
 
 // Configuración
 builder.Configuration
